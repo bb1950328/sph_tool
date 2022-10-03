159a09c46172403a7779d9185705354d306f4c87
bb1950328 <bb1950328@gmail.com>
2022-10-03 20:54:20 +0200

    start with ballistics calculator add entries for input data enter all data from ABAQUEs

--- src/App.vue
+++ src/App.vue
@@ -10,17 +10,22 @@
 <script>
 import PointList from "@/components/PointList";
 import CoordinateCalculations from "@/components/CoordinateCalculations";
-
+import BallisticsCalculator from "@/components/BallisticsCalculator";
 
 export default {
   name: 'App',
   components: {
     PointList,
     CoordinateCalculations,
+    BallisticsCalculator,
   },
   data() {
     let currentTool = "PointList";
-    let allTools = [["PointList", "Punkteliste"], ["CoordinateCalculations", "Koordinatenberechnungen"]];
+    let allTools = [
+      ["PointList", "Punkteliste"],
+      ["CoordinateCalculations", "Koordinatenberechnungen"],
+      ["BallisticsCalculator", "Ballistikrechner"],
+    ];
     if (window.location.hash) {
       for (let i = 0; i < allTools.length; i++) {
         let fragment = window.location.hash.slice(1);
---
+++ src/components/BallisticsCalculator.vue
@@ -0,0 +1,76 @@
+<template>
+  <form>
+    <div class="mb-3">
+      <label for="distanceInput" class="form-label">Distanz</label>
+      <input id="distanceInput" type="number" class="form-control" v-model="distance">
+    </div>
+    <div class="mb-3 row">
+      <div class="col-6">
+        <label for="windSpeedInput" class="form-label">Windgeschwindigkeit</label>
+        <input id="windSpeedInput" type="number" class="form-control" v-model="windSpeed">
+      </div>
+      <div class="col-6">
+        <label for="windDirectionInput" class="form-label">Windrichtung (Uhr)</label>
+        <input id="windDirectionInput" type="number" class="form-control" v-model="windClock">
+      </div>
+    </div>
+    <div class="mb-3 row">
+      <div class="col-6">
+        <label for="airPressureInput" class="form-label">Luftdruck</label>
+        <input id="airPressureInput" type="number" class="form-control" v-model="airPressure">
+      </div>
+      <div class="col-6">
+        <label for="temperatureInput" class="form-label">Temperatur</label>
+        <input id="temperatureInput" type="number" class="form-control" v-model="temperature">
+      </div>
+    </div>
+  </form>
+  <div>
+    <table>
+      <tr>
+        <td>Höhe</td>
+        <td>{{ formattedElevationClicks }}</td>
+      </tr>
+      <tr>
+        <td>Seite</td>
+        <td>{{ formattedWindageClicks }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BallisticsCalculator",
+  data() {
+    return {
+      distance: 100,
+      windSpeed: 0,
+      windClock: 0,
+      airPressure: 950,
+      temperature: 25,
+
+      resultElevationClicks: 0,
+      resultWindageClicks: 0,
+    }
+  },
+  computed: {
+    formattedElevationClicks() {
+      return (this.resultElevationClicks <= 0 ? "" : "+") + this.resultElevationClicks;
+    },
+    formattedWindageClicks() {
+      let word = "";
+      if (this.resultWindageClicks < 0) {
+        word = "negativ ";
+      } else if (this.resultWindageClicks > 0) {
+        word = "positiv ";
+      }
+      return word + Math.abs(this.resultWindageClicks);
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
